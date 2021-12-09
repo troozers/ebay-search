7671c5ec74f37c08e1c6dde627361434115cb6bf
andy stewart <andy@troozers.com>
2021-12-09 15:19:16 +0000

    Removed testclass.py and updated the main script to prompt for parameters

--- ebay-search.py
+++ ebay-search.py
@@ -1,100 +1,115 @@
 #!/usr/bin/env python3
+import click
+import json
 import pandas as pd
 from datetime import datetime
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 
-pd.set_option('max_colwidth', 1000)
-pd.set_option('max_rows', 1000)
-APPLICATION_ID = "AndyStew-searchne-PRD-a7c924ccb-247abd5b"
-FILENAME = "/home/andy/Downloads/eBay-Results.md"
-TIMESTAMP = datetime.now().strftime('%A - %d-%b-%Y - %H:%M')
 
-search = {
-    'keywords': 'atari xl',
-    'categoryId': ['162075'],
-    'itemFilter': [
-        {'name': 'LocatedIn', 'value': 'GB'},
-        {'name': 'MaxPrice', 'value': '100'}
-    ],
-    'sortOrder': 'StartTimeNewest',
-}
+class eBayConnect(object):
 
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.search = dict()
+        self.timestamp = datetime.now().strftime('%A - %d-%b-%Y - %H:%M')
+        pd.set_option('max_colwidth', 1000)
+        pd.set_option('max_rows', 1000)
 
-def get_results(payload):
-    try:
-        api = Finding(siteid="EBAY-GB", appid=APPLICATION_ID, config_file=None)
-        response = api.execute('findItemsAdvanced', payload)
-        return response.dict()
-    except ConnectionError as e:
-        print(e)
-        print(e.response.dict())
+    def look_for(self, search):
+        self.search = search
+        try:
+            api = Finding(siteid="EBAY-GB", appid=self.api_key, config_file=None)
+            response = api.execute('findItemsAdvanced', self.search)
+            return response.dict()
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
 
+    @staticmethod
+    def total_pages(results):
+        if results:
+            return int(results.get('paginationOutput').get('totalPages'))
+        else:
+            return
 
-def get_total_pages(results):
-    if results:
-        return int(results.get('paginationOutput').get('totalPages'))
-    else:
-        return
+    def get_results(self, results):
+        total_pages = self.total_pages(results)
+        items_list = results['searchResult']['item']
 
+        i = 2
+        while i <= total_pages:
+            self.search['paginationInput'] = {'entriesPerPage': 100, 'pageNumber': i}
+            results = self.look_for(self.search)
+            items_list.extend(results['searchResult']['item'])
+            i += 1
 
-def search_ebay(payload):
-    """Search eBay with --search keywords with --category and return a list into a markdown document"""
-    results = get_results(payload)
-    total_pages = get_total_pages(results)
-    items_list = results['searchResult']['item']
+        df_items = pd.DataFrame(columns=['title', 'viewItemURL', 'galleryURL', 'location', 'postalCode',
+                                         'listingType', 'buyItNowAvailable', 'currentPrice', 'endTime'])
 
-    i = 2
-    while i <= total_pages:
-        payload['paginationInput'] = {'entriesPerPage': 100, 'pageNumber': i}
-        results = get_results(payload)
-        items_list.extend(results['searchResult']['item'])
-        i += 1
+        for item in items_list:
+            row = {
+                'title': item.get('title'),
+                'viewItemURL': item.get('viewItemURL'),
+                'galleryURL': item.get('galleryURL'),
+                'location': item.get('location'),
+                'postalCode': item.get('postalCode'),
+                'listingType': item.get('listingInfo').get('listingType'),
+                'buyItNowAvailable': item.get('listingInfo').get('buyItNowAvailable'),
+                'currentPrice': f"{float(item.get('sellingStatus').get('currentPrice').get('value')):.2f}",
+                'endTime': datetime.fromisoformat(item.get('listingInfo').get('endTime').replace("Z", "+00:00")),
+            }
 
-    df_items = pd.DataFrame(columns=['title', 'viewItemURL', 'galleryURL', 'location', 'postalCode',
-                                     'listingType', 'buyItNowAvailable', 'currentPrice', 'endTime'])
+            df_items = df_items.append(row, ignore_index=True)
 
-    for item in items_list:
-        row = {
-            'title': item.get('title'),
-            'viewItemURL': item.get('viewItemURL'),
-            'galleryURL': item.get('galleryURL'),
-            'location': item.get('location'),
-            'postalCode': item.get('postalCode'),
-            'listingType': item.get('listingInfo').get('listingType'),
-            'buyItNowAvailable': item.get('listingInfo').get('buyItNowAvailable'),
-            'currentPrice': f"{float(item.get('sellingStatus').get('currentPrice').get('value')):.2f}",
-            'endTime': datetime.fromisoformat(item.get('listingInfo').get('endTime').replace("Z", "+00:00")),
-        }
-
-        df_items = df_items.append(row, ignore_index=True)
-
-    return df_items
+        return df_items
 
 
-if __name__ == '__main__':
-    ebay_results = search_ebay(search)
-
+def write_results(filename, items):
     # Output results to a markdown file
-    with open(FILENAME, 'w') as fn:
+    with open(filename, 'w') as fn:
         print("# eBay Search Results", file=fn)
-        print(f"## {TIMESTAMP}", file=fn)
+        print(f"## {ebay.timestamp}", file=fn)
+        print(f"Searching for:", file=fn)
+        print(f"```json\n{json.dumps(ebay.search, indent=4)}\n```", file=fn)
         print("| Item | Image | Location | Price | Type | BiN? | End |", file=fn)
         print("| ---- | ----- | -------- | ----: | :--: | :--: | :-: |", file=fn)
 
-        for key, val in ebay_results.iterrows():
+        for key, item in items.iterrows():
             # Check Buy it Now Status
-            if val['listingType'] == "Auction":
-                BiN = "Yes" if val['buyItNowAvailable'] == "true" else "No"
+            if item['listingType'] == "Auction":
+                buyitnow = "Yes" if item['buyItNowAvailable'] == "true" else "No"
             else:
-                BiN = "Yes"
+                buyitnow = "Yes"
 
             # Output each row of results
-            print(f"| [{val['title'][0:25]}...]({val['viewItemURL']} \"{val['title']}\")  "
-                  f"| ![image]({val['galleryURL']}) "              
-                  f"| {val['location'].removesuffix(',United Kingdom')} "
-                  f"| `{val['currentPrice']}` " 
-                  f"| {val['listingType']} "
-                  f"| {BiN} "
-                  f"| `{val['endTime'].strftime('%Y-%m-%d %H:%M')}` |",
+            print(f"| [{item['title'][0:25]}...]({item['viewItemURL']} \"{item['title']}\")  "
+                  f"| ![image]({item['galleryURL']}) "
+                  f"| {item['location'].removesuffix(',United Kingdom')} "
+                  f"| `{item['currentPrice']}` "
+                  f"| {item['listingType']} "
+                  f"| {buyitnow} "
+                  f"| `{item['endTime'].strftime('%Y-%m-%d %H:%M')}` |",
                   file=fn)
+
+
+@click.command()
+@click.option('--keywords', '-k', required=True, help='Keywords to search eBay for.')
+@click.option('--maxprice', '-m', default='999.99', help='Maximum price willing to pay for the item. (default=999.99)')
+@click.option('--category', '-c', required=True, help='Which category id to look in.')
+@click.option('--filename', '-o', required=True, help='Name of markdown file to create.')
+def get_results(keywords, maxprice, category, filename):
+    """Utility to search eBay for --keywords and return the items in a markdown file."""
+
+    my_stuff = dict(keywords=keywords, categoryId=[category], itemFilter=[{'name': 'LocatedIn', 'value': 'GB'},
+                                                                          {'name': 'MaxPrice', 'value': maxprice}],
+                    sortOrder='startTimeNewest')
+
+    found = ebay.look_for(my_stuff)
+    data = ebay.get_results(found)
+    write_results(filename, data)
+
+
+if __name__ == '__main__':
+    ebay = eBayConnect(api_key='AndyStew-searchne-PRD-a7c924ccb-247abd5b')
+    get_results()
